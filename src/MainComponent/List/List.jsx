0298868256fb9f310dd6d12616880524b6e158a0
RagavarshiniC <113581980+RagavarshiniC@users.noreply.github.com>
2024-02-04 13:19:49 +0530

    Update List.jsx

--- src/MainComponent/List/List.jsx
+++ src/MainComponent/List/List.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import styles from '../../Styles/List.module.css';
-import CircleInitials from '../CircleInitials';
+import styles from '../../Styles/List.module.css'
+import CircleInitials from '../CircleInitials.jsx'
 export default function List({ color, name, onClick }) {
   const circleStyle = {
     backgroundColor: color,
   };
 
   const getInitials = (groupName) => {
+    if (typeof groupName !== 'string' || groupName.trim() === '') {
+      // Handle cases where groupName is not a string or an empty string
+      return '';
+    }
+  
     const words = groupName.split(' ');
-
+  
     if (words.length === 1) {
       // For single-word group names, return only the first letter
       return words[0]?.charAt(0).toUpperCase() || '';
@@ -20,10 +25,12 @@ export default function List({ color, name, onClick }) {
       return `${firstLetter.toUpperCase()}${lastLetter.toUpperCase()}`;
     }
   };
+  
   const initials = getInitials(name);
+  
 
   return (
-    <div className="listt" style={{ margin: '20px' }} onClick={onClick}>
+    <div style={{ margin: '20px' }} onClick={onClick}>
       <div className={styles.ListBody}>
         <div className={styles.Listdiv}>
           <CircleInitials initials={initials} color={color}/>
